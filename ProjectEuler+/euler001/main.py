909deac80e076367c20dc2b7ce0447d728dd790c
Marvin Kurland <bugemarvin@outlook.com>
2023-06-21 18:27:54 +0300

    working with natural numbers

--- ProjectEuler+/euler001/main.py
+++ ProjectEuler+/euler001/main.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python3
 
-import math
-import os
-import random
-import re
-import sys
-
 
 if __name__ == '__main__':
-    t = int(input().strip())
-    total = 0
-    numbers = []
-    for i in range(1, t):
-        if i % 3 == 0 or i % 5 == 0:
-            total += i
-            numbers.append(i)
-    print("sum of the following {} is: {}".format(numbers, total))
+    def multiples(j):
+      '''
+      finding the multiples of 3 or 5.
+      time complexity of O(1)
+      '''
+
+      j -= 1 # confirming that `j` is always less of the number
+      threes = (3 * (j // 3) * ((j // 3) + 1)) // 2 # calculating the sum multiples 3
+      fives = (5 * (j // 5) * ((j // 5) + 1)) // 2 # calculating the sum multiples 5
+      fifteen = (15 * (j // 15) *((j // 15) + 1)) // 2 # Calculating the sum of both 3 and 5
+      total = threes + fives - fifteen
+      return total
